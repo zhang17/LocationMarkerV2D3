03c6d72ffd377dce04f74f26709c08d7daf87d52
zhang17 <zhang17@umd.edu>
2016-01-11 11:02:38 -0500

    ActionBar

--- app/src/main/java/com/first_teck/locationmarkerv2drawer/MainActivity.java
+++ app/src/main/java/com/first_teck/locationmarkerv2drawer/MainActivity.java
@@ -4,6 +4,7 @@ package com.first_teck.locationmarkerv2drawer;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawerSetUp();
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 
     @Override
